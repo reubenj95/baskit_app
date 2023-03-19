05d9b11c82e2efe43e3c1bc787ef062049e1aad8
Reuben Hawthorne-Jensen <103969792+reubenj95@users.noreply.github.com>
2023-03-20 05:49:42 +1300

    Minor refactor to DB functions to make param names explicit

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,9 @@
 import { join } from 'node:path'
 import express from 'express'
 import { rmSync } from 'node:fs'
+import pantryRoutes from './routes/pantry'
 
 const server = express()
-
 server.use(express.static(join(__dirname, 'public')))
 
 server.get('*', (req, res) => {
